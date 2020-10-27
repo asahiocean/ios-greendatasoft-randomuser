de13729eae73c8bd16fd7ee5a3b703bacf865aa0
AsahiOcean <asahiocean@icloud.com>
2020-10-27 19:21:43 +0500

    init

---
+++ Project/Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by Sergey Fedotov on 27.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
