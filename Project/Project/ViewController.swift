384dd1b04370c4718b3b2796c48acf9ae5bfc98f
AsahiOcean <asahiocean@icloud.com>
2020-10-27 20:05:36 +0500

    Start

--- Project/Project/ViewController.swift
+++ Project/Project/ViewController.swift
@@ -11,9 +11,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.layer.backgroundColor = #colorLiteral(red: 0.0170332063, green: 0.2035003901, blue: 0.1973262429, alpha: 1)
+        
+        // Test for Bitrise + TestFlight
+        let indicator = UIActivityIndicatorView()
+        indicator.center = view.center
+        indicator.style = .large
+        indicator.color = #colorLiteral(red: 0.8300367594, green: 0.711122334, blue: 0.5887681246, alpha: 1)
+        indicator.startAnimating()
+        view.addSubview(indicator)
     }
-
-
 }
 
