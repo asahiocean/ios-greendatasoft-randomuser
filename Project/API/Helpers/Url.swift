5e88b96e69db85e7a666570070572f3c6d610ac0
AsahiOcean <asahiocean@icloud.com>
2020-11-11 23:52:01 +0500

    del

--- Project/API/Helpers/Url.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-enum Url: String, CaseIterable {
-    case getUsers = "https://randomuser.me/api/?results="
-    case post = "https://ptsv2.com/t/jtdxs-1605082934/post"
-}
--- Project/Helpers/Extensions/UIView/removeSubviews.swift
+++
@@ -1,7 +0,0 @@
-import UIKit.UIView
-
-extension UIView {
-    func removeSubviews() {
-        self.subviews.forEach { $0.removeFromSuperview() }
-    }
-}
