94e761f28c96f861fbaaa98f290df6b3055fc682
AsahiOcean <asahiocean@icloud.com>
2020-11-18 00:44:53 +0500

    x

--- Project/Project/Controllers/TableViewController/Extensions/didSelectRowAt.swift
+++ Project/Project/Controllers/TableViewController/Extensions/didSelectRowAt.swift
@@ -4,6 +4,14 @@ import UIKit.UITableView
 extension TableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        didSelectRowAt_UserinfoVC(indexPath)
+        if let result = storage.database?.results[indexPath.row],
+           let nav = navigationController {
+            let name = UserinfoVC.nibName
+            let bundle = Bundle(for: UserinfoVC.self)
+            if let userinfo = UINib(nibName: name, bundle: bundle).instantiate(withOwner: nil, options: nil).first as? UserinfoVC {
+                userinfo.setUserInfo(result)
+                nav.pushViewController(userinfo, animated: true)
+            }
+        }
     }
 }
