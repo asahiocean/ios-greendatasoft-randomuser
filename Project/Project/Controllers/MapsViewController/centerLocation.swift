104d04c7fd10961792a50fa2bc0415a4168a6a2f
AsahiOcean <asahiocean@icloud.com>
2021-01-07 19:47:47 +0500

    1610030863

--- Project/Project/Controllers/MapsViewController/centerLocation.swift
+++ Project/Project/Controllers/MapsViewController/centerLocation.swift
@@ -2,11 +2,11 @@ import Foundation
 import MapKit
 
 extension MKMapView {
-  func centerLocation(_ location: CLLocation, regionRadius: CLLocationDistance = 1000) {
-    let coordinateRegion = MKCoordinateRegion(
-      center: location.coordinate,
-      latitudinalMeters: regionRadius,
-      longitudinalMeters: regionRadius)
-      setRegion(coordinateRegion, animated: true)
-  }
+    public func centerLocation(_ location: CLLocation, regionRadius: CLLocationDistance = 1000) {
+        let coordinateRegion = MKCoordinateRegion(
+            center: location.coordinate,
+            latitudinalMeters: regionRadius,
+            longitudinalMeters: regionRadius)
+        setRegion(coordinateRegion, animated: true)
+    }
 }
