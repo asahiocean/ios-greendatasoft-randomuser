2dcbb4371129eb091263dbd434ea67497a85cf79
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2021-01-07 19:17:44 +0500

    1610029058

--- Project/Project/SceneDelegate.swift
+++ Project/Project/SceneDelegate.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
     var navigationController: NavigationController!
     
@@ -15,18 +15,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                 window.makeKeyAndVisible()
             }
         }
-
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) { }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) { }
-
+    
     func sceneWillResignActive(_ scene: UIScene) { }
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) { }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
