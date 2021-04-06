5ac31f7d6e54ddbf12265f777e1776912cd55905
AsahiOcean <asahiocean@icloud.com>
2021-04-06 06:12:11 +0500

    minor

--- Project/Project/Handler/Handler.swift
+++ Project/Project/Handler/Handler.swift
@@ -31,3 +31,20 @@ final class Handler {
     
     fileprivate init() { }
 }
+extension Handler {
+    func getBackup() {
+        guard storage.database == nil else { return }
+        
+        if let keys = storage.cache.allKeys() as? [String], keys.isEmpty == false {
+            for key in keys where key.hasPrefix(jsonDataKey) {
+                guard let data = storage.cache.data(forKey: key) else { return }
+                self.jsonData(data)
+            }
+        } else {
+            storage.getCoreData(JsondataEntity.self, output: { array -> Void in
+                guard let last = array.last, let data = last.jsonData else { return }
+                self.jsonData(data)
+            })
+        }
+    }
+}
